ab4c8675e7548774c01417788a31c6e4a51976fc
mauliwanave <mauliwanave2005@gmail.com>
2026-04-20 22:38:36 +0530

    Fix Render deploy: upgrade pip for Pillow wheel support

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip first to ensure wheel support for Pillow
+python -m pip install --upgrade pip
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
